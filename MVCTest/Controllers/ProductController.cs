931beec94c8b723c7bce11a54472dca2e8bfa7c2
Ritika Navrang <ritikanavrang.bs@gmail.com>
2024-08-29 12:41:36 +0530

    Add project files.

---
+++ MVCTest/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCTest.Models;
+using X.PagedList.Extensions;
+
+namespace MVCTest.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+
+        public IActionResult GetProducts(string titleSearch, DateTime? dateSearch, int pageNumber = 1)
+        {
+            var products = _context.Products.ToList();
+
+            if (!string.IsNullOrEmpty(titleSearch))
+            {
+                products = products.Where(p => p.Title.Contains(titleSearch, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            if (dateSearch.HasValue)
+            {
+                products = products.Where(p => p.Date.Date == dateSearch.Value.Date).ToList();
+            }
+
+            int pageSize = 10;
+            var pagedList = products.ToPagedList(pageNumber, pageSize);
+            return View(pagedList);
+        }
+
+        public IActionResult AddProducts()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProducts(List<Product> models)
+        {
+            foreach (var model in models)
+            {
+                if (model.Image != null)
+                {
+                    var imagePath = SaveImage(model.Image);
+                    model.ImagePath = "/images/" + Path.GetFileName(imagePath);
+                }
+                _context.Add(model);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("GetProducts");
+        }
+
+        private string SaveImage(IFormFile image)
+        {
+            var fileName = Guid.NewGuid().ToString() + "_" + image.FileName;
+
+            var imagePath = "/images/" + fileName;
+            var physicalPath = Path.Combine(Directory.GetCurrentDirectory(), "images", fileName);
+            using (var stream = new FileStream(physicalPath, FileMode.Create))
+            {
+                image.CopyTo(stream);
+            }
+
+            return imagePath;
+        }
+    }
+}
---
+++ MVCTest/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace MVCTest.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required, StringLength(250, ErrorMessage = "Description should not be greater than 250 words.")]
+        public string Description { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public string? ImagePath { get; set; }
+        [NotMapped]
+        public IFormFile? Image { get; set; }
+    }
+}
+
